9ed6e010c32b8ad14db460865c6f935f0d0c8285
Cunha-Renato <cunhamrenato@gmail.com>
2026-03-30 15:09:36 -0300

    switched to react

--- convex/schema.ts
+++
@@ -1,8 +0,0 @@
-import { defineSchema, defineTable } from "convex/server";
-import { v } from "convex/values";
-
-export default defineSchema({
-	test: defineTable({
-		value: v.number(),
-	}).index("by_value", ["value"]),
-});
